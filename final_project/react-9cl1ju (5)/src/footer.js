925e6ba79f6e843106a336f6222a7f0dfe4a578a
cykojima <122499702+cykojima@users.noreply.github.com>
2023-12-04 20:54:58 -0800

    final project submission

---
+++ final_project/react-9cl1ju (5)/src/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+export default function Footer() {
+  return (
+    <div className="footerContainer">
+      <div className="footerTitle">MATCH @ USC</div>
+      <div className="footerEmail">MATCH@gmail.com</div>
+    </div>
+  );
+}
---
+++ final_project/react-9cl1ju (5)/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app.js';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
